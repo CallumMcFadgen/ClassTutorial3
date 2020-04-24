385548079ef29a066f2c87922c39ab26b1f23bb1
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-04-24 14:11:34 +1200

    Working on Step 4 but having a bug with data not being visable in the windows client form

--- Gallery3/Gallery3WinForm/Gallery3WinForm/ServiceClient.cs
+++ Gallery3/Gallery3WinForm/Gallery3WinForm/ServiceClient.cs
@@ -9,7 +9,7 @@ namespace Gallery3WinForm
 {
     public static class ServiceClient
     {
-       internal async static Task<List<string>> GetArtistNamesAsync()
+        internal async static Task<List<string>> GetArtistNamesAsync()
         {
             using (HttpClient lcHttpClient = new HttpClient())
                 return JsonConvert.DeserializeObject<List<string>>
