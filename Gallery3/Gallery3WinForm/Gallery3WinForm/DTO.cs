3f032c57b4806adb7481a1e5ec37e3edc0f5d9c0
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-04-24 21:10:17 +1200

    Step 5 completed and working as expected

---
+++ Gallery3/Gallery3WinForm/Gallery3WinForm/DTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3WinForm
+{
+    // ARTIST DTO
+    public class clsArtist
+    {
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public string Phone { get; set; }
+
+        //public List<clsAllWork> WorksList { get; set; }
+    }
+
+
+
+}
--- Gallery3/Gallery3WinForm/Gallery3WinForm/ServiceClient.cs
+++ Gallery3/Gallery3WinForm/Gallery3WinForm/ServiceClient.cs
@@ -9,11 +9,21 @@ namespace Gallery3WinForm
 {
     public static class ServiceClient
     {
+        //GET ALL ARTIST NAMES AS STRING
         internal async static Task<List<string>> GetArtistNamesAsync()
         {
             using (HttpClient lcHttpClient = new HttpClient())
                 return JsonConvert.DeserializeObject<List<string>>
                 (await lcHttpClient.GetStringAsync("http://localhost:60064/api/gallery/GetArtistNames/"));
+        }
+
+        // GET AN ARTIST AS DTO
+        internal async static Task<clsArtist> GetArtistAsync(string prArtistName)
+        {
+            using (HttpClient lcHttpClient = new HttpClient())
+                return JsonConvert.DeserializeObject<clsArtist>
+                (await lcHttpClient.GetStringAsync
+                ("http://localhost:60064/api/gallery/GetArtist?Name=" + prArtistName));
         }
     }
 }
